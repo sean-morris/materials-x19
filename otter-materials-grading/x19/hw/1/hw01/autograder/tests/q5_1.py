21adfd4e30ce4b7c3bfb9ff314a86385f20b4c3d
Sean Morris <sean.smorris@berkeley.edu>
2021-10-11 16:39:53 -0700

    adding otter test zips to repo

---
+++ otter-materials-grading/x19/hw/1/hw01/autograder/tests/q5_1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q5_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(biggest_change,(int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> biggest_change == 57\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
